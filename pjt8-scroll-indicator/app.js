cfd75aeea7173cea44886ab7c1eabfe0384061a3
Mui <muides_@outlook.com>
2024-11-21 15:32:51 +0300

    scroll indcator

---
+++ pjt8-scroll-indicator/app.js
@@ -0,0 +1,10 @@
+let indicator = document.querySelector(".scroll");
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', scroll);
+
+function scroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    let scrolled = (scrollTop / scrollHeight) * 100;
+    indicator.style.width = `${scrolled}%`;
+}
